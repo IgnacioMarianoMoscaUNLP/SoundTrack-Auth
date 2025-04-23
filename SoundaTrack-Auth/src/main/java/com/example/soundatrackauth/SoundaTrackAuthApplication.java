b4bd91c6de5ea720182613733bbceae4cb0f4de8
ignacio mariano <you@example.com>
2025-04-23 16:54:03 -0300

    proyect initialization

---
+++ SoundaTrack-Auth/src/main/java/com/example/soundatrackauth/SoundaTrackAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.soundatrackauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundaTrackAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoundaTrackAuthApplication.class, args);
+    }
+
+}
---
+++ SoundaTrack-Auth/src/test/java/com/example/soundatrackauth/SoundaTrackAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soundatrackauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundaTrackAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
