25439f2b43bfb917579962dd1cacdade7348ff42
ignacio mariano <you@example.com>
2025-04-23 18:20:34 -0300

    no commments

---
+++ auth-backend/src/test/java/com/soundtrack/authbackend/SoundTrackAuthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soundtrack.authbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundTrackAuthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
