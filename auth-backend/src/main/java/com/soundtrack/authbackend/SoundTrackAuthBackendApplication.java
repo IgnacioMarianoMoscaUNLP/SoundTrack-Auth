68fa0e1bdf343b6000cf3814de6e095d6cd93b80
ignacio mariano <you@example.com>
2025-04-23 18:17:14 -0300

    project Structure

---
+++ auth-backend/src/main/java/com/soundtrack/authbackend/SoundTrackAuthBackendApplication.java
@@ -0,0 +1,13 @@
+package com.soundtrack.authbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundTrackAuthBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundTrackAuthBackendApplication.class, args);
+	}
+
+}
