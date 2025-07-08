7d9c67d9d920e8f32fa39b67e8d3d22729e9a0d2
Ignacio Mariano <ignaciomarianomosca@gmail.com>
2025-07-08 18:31:37 -0300

    config has been modified

--- auth-backend/src/main/java/com/soundtrack/authbackend/config/SecurityConfig.java
+++ auth-backend/src/main/java/com/soundtrack/authbackend/config/SecurityConfig.java
@@ -2,27 +2,46 @@ package com.soundtrack.authbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+import com.soundtrack.authbackend.repository.UserRepository;
+import lombok.AllArgsConstructor;
 
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 @Configuration
-@EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+  
+    private final JwtConfig jwtConfig;
+    private final UserRepository userRepository;
+  
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); // Bean aquí mismo
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+     return    http
+               .csrf(AbstractHttpConfigurer::disable)
+               .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                    .requestMatchers("/auth/**").permitAll() 
+                    .anyRequest().authenticated())
+                    .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .oauth2ResourceServer(configurer -> configurer.jwt(Customizer.withDefaults()))
+                        .build();
+    }
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withSecretKey(jwtConfig.getSecretKey())
+                .build();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/api/auth/**").permitAll()
-                .anyRequest().authenticated();
-        return http.build();
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
